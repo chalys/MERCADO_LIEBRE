60e2df7233a65abe11c92acb3e62468dca4c3635
Carlos Roberto Arando Llanos <chalys.all@gmail.com>
2023-12-21 00:06:44 -0400

    Created header main footer sections in home.html

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const express = requiere('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const port = 3030;
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname, './views/home.html'));
+app.use(express.static('public'))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/home.html"));
 });
 
-app.listen(port, ()=>console.log(`http://localhost${port}`))
+app.listen(port, () => console.log(`http://localhost:${port}`));
